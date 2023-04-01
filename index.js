8beeaa624344917e77e48758756244397437a428
digitalaltar <moon@digitalaltar.com>
2023-03-31 18:52:31 -0700

    Updating Safari support

--- index.js
+++ index.js
@@ -69,15 +69,50 @@ Promise.all(promises).then(() => {
 
   // load the bottom image
   const bottomImg = new Image();
-    const randomBottomIndex = Math.floor($fx.rand() * imgUrls.length); // use fx(rand)
-    const bottomImgUrl = imgUrls[randomBottomIndex];
-    const bottomHueRotate = Math.floor($fx.rand() * 360); // use fx(rand)
+  const randomBottomIndex = Math.floor($fx.rand() * imgUrls.length); // use fx(rand)
+  const bottomImgUrl = imgUrls[randomBottomIndex];
 
   bottomImg.src = bottomImgUrl;
 
   bottomImg.onload = function() {
-    ctx.filter = `hue-rotate(${bottomHueRotate}deg) brightness(100%) contrast(150%)`;
-      ctx.drawImage(bottomImg, 0, 0);
+
+      // draw the bottom img
+      ctx.save(); // save the current context state
+      ctx.globalAlpha = .3 + $fx.rand() * (.9 - .3); // use fx(rand) to set a random opacity
+      ctx.drawImage(bottomImg, 0, 0); // draw the top image
+
+      // Create a new canvas to use as a buffer
+      const bufferCanvas = document.createElement('canvas');
+      bufferCanvas.width = canvas.width;
+      bufferCanvas.height = canvas.height;
+      const bufferCtx = bufferCanvas.getContext('2d');
+
+      // Get the image data from the canvas
+      const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+      // Loop through the image data and modify the hue of each pixel
+      for (let i = 0; i < imageData.data.length; i += 4) {
+        const r = imageData.data[i];
+        const g = imageData.data[i + 1];
+        const b = imageData.data[i + 2];
+
+        // Convert RGB to HSL
+        const [h, s, l] = rgbToHsl(r, g, b);
+
+      // Randomize the hue modification
+        const deltaH = ($fx.rand() * 0.5 + 0.8).toFixed(1);
+
+        // Modify the hue
+        const newH = (h + deltaH) % 1;
+
+        // Convert back to RGB
+        const [newR, newG, newB] = hslToRgb(newH, s, l);
+
+        // Set the new pixel values
+        imageData.data[i] = newR;
+        imageData.data[i + 1] = newG;
+        imageData.data[i + 2] = newB;
+      }
 
       // now load the top image
       let randomTopIndex;
@@ -86,17 +121,54 @@ Promise.all(promises).then(() => {
       } while (randomTopIndex === randomBottomIndex);
 
     const topImgUrl = imgUrls[randomTopIndex];
-    const topHueRotate = Math.floor($fx.rand() * 360); // use fx(rand)
     const topImg = new Image();
 
     topImg.src = topImgUrl;
-      topImg.onload = function() {
+    topImg.onload = function() {
 
+      // draw the top image
       ctx.save(); // save the current context state
-      ctx.globalAlpha = 0.6; // set the opacity to 60%
-      ctx.filter = `hue-rotate(${topHueRotate}deg) brightness(100%) contrast(150%)`; // set other filters
+      ctx.globalAlpha = .2 + $fx.rand() * (.8 - .2); // set a random opacity between 0.2 and 0.8
       ctx.drawImage(topImg, 0, 0); // draw the top image
-      ctx.restore(); // restore the context state to its previous state
+
+      // Create a new canvas to use as a buffer
+      const bufferCanvas = document.createElement('canvas');
+      bufferCanvas.width = canvas.width;
+      bufferCanvas.height = canvas.height;
+      const bufferCtx = bufferCanvas.getContext('2d');
+
+      // Get the image data from the canvas
+      const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+      // Loop through the image data and modify the hue of each pixel
+      for (let i = 0; i < imageData.data.length; i += 4) {
+        const r = imageData.data[i];
+        const g = imageData.data[i + 1];
+        const b = imageData.data[i + 2];
+
+        // Convert RGB to HSL
+        const [h, s, l] = rgbToHsl(r, g, b);
+
+      // Randomize the hue modification
+        const deltaH = ($fx.rand() * 0.5 + 0.8).toFixed(1);
+
+        // Modify the hue
+        const newH = (h + deltaH) % 1;
+
+        // Convert back to RGB
+        const [newR, newG, newB] = hslToRgb(newH, s, l);
+
+        // Set the new pixel values
+        imageData.data[i] = newR;
+        imageData.data[i + 1] = newG;
+        imageData.data[i + 2] = newB;
+      }
+
+  // Put the modified image data onto the buffer canvas
+  bufferCtx.putImageData(imageData, 0, 0);
+
+  // Draw the modified image onto the original canvas
+  ctx.drawImage(bufferCanvas, 0, 0);
 
       // draw lines
       drawLines();
@@ -117,7 +189,7 @@ Promise.all(promises).then(() => {
             drawSquares();
           }
       }
-      }
+    }
   };
 
   function drawLines() {
@@ -203,5 +275,57 @@ Promise.all(promises).then(() => {
         const y = Math.floor($fx.rand() * canvas.height); // use fx(rand)
       ctx.fillRect(x, y, size, size);
     }
+  }
+
+  // Utility functions for RGB to HSL conversion
+  function rgbToHsl(r, g, b) {
+    r /= 255;
+    g /= 255;
+    b /= 255;
+
+    const max = Math.max(r, g, b);
+    const min = Math.min(r, g, b);
+
+    let h, s, l = (max + min) / 2;
+
+    if (max === min) {
+      h = s = 0; // achromatic
+    } else {
+      const d = max - min;
+      s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+      switch (max) {
+        case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+        case g: h = (b - r) / d + 2; break;
+        case b: h = (r - g) / d + 4; break;
+      }
+    h /= 6;
+  }
+
+  return [h, s, l];
+  }
+
+  function hslToRgb(h, s, l) {
+  let r, g, b;
+
+  if (s === 0) {
+  r = g = b = l; // achromatic
+  } else {
+  const hue2rgb = (p, q, t) => {
+  if (t < 0) t += 1;
+  if (t > 1) t -= 1;
+  if (t < 1/6) return p + (q - p) * 6 * t;
+  if (t < 1/2) return q;
+  if (t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+  return p;
+  };
+
+  const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+  const p = 2 * l - q;
+  r = hue2rgb(p, q, h + 1/3);
+  g = hue2rgb(p, q, h);
+  b = hue2rgb(p, q, h - 1/3);
+  }
+
+  return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
   }
 });
